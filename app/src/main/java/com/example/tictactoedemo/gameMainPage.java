f139a47f88191a8bab01cf63e1210167c1166bad
jg52can <jobingrg52@gmail.com>
2020-11-14 21:58:54 -0500

    Program done.

--- app/src/main/java/com/example/tictactoedemo/gameMainPage.java
+++ app/src/main/java/com/example/tictactoedemo/gameMainPage.java
@@ -1,5 +1,6 @@
 package com.example.tictactoedemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,14 +8,63 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class gameMainPage extends AppCompatActivity {
     private Button pc, online, friends,exit,profile;
+    FirebaseUser user;
+    String userID;
+    private DatabaseReference reference;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_main_page);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User profile = snapshot.getValue(User.class);
+                if(profile != null){
+                    User user = new User();
+                    user.status = true;
+                    reference.child(userID).child("status").setValue(user.status);
+                    String names = profile.getUsername();
+                    String emails = profile.getEmail();
+
+                    Online onuser = new Online(names,emails);
+                    FirebaseDatabase.getInstance().getReference("Online").
+                            child(FirebaseAuth.getInstance().getCurrentUser().getUid()).
+                            setValue(onuser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(gameMainPage.this, "Something went wrong. Restart the app.", Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         pc  = (Button) findViewById(R.id.btn_user_computer);
         online  = (Button) findViewById(R.id.btn_user_play_online);
